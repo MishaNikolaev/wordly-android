0cad34cdfeac424ab6f23a14020539559010fffd
Mikhail Nikolaev <nmichail154@mail.ru>
2026-07-24 23:16:36 +0700

    Wordly-19: обновил компонент

--- features/review/src/main/java/com/nmichail/wordly/android/features/review/presentation/ReviewComponent.kt
+++ features/review/src/main/java/com/nmichail/wordly/android/features/review/presentation/ReviewComponent.kt
@@ -2,7 +2,7 @@ package com.nmichail.wordly.android.features.review.presentation
 
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.Value
-import com.nmichail.wordly.android.features.review.domain.entity.ReviewQuestion
+import com.nmichail.wordly.android.features.review.domain.entity.ReviewWord
 
 interface ReviewComponent {
 
@@ -10,14 +10,40 @@ interface ReviewComponent {
 
 	fun handleClose()
 
+	fun handleRetry()
+
 	fun handlePlayAudio()
 
 	fun handleSelectOption(optionId: String)
 
-	data class State(
-		val question: ReviewQuestion,
-		val selectedOptionId: String?,
-	)
+	fun handleContinue()
+
+	fun handleFinish()
+
+	sealed interface State {
+
+		data object Loading : State
+
+		data object Error : State
+
+		data class InProgress(
+			val words: List<ReviewWord>,
+			val currentIndex: Int,
+			val currentWord: ReviewWord,
+			val totalCount: Int,
+			val progressIndex: Int,
+			val selectedOptionId: String?,
+			val isAnswerRevealed: Boolean,
+			val isCorrect: Boolean,
+			val correctCount: Int,
+			val isSubmitting: Boolean,
+		) : State
+
+		data class Finished(
+			val totalCount: Int,
+			val correctCount: Int,
+		) : State
+	}
 
 	sealed interface Label {
 
