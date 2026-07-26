2b483015b260af6a813651be98109bce9b852e04
Mikhail Nikolaev <nmichail154@mail.ru>
2026-07-26 21:36:56 +0700

    Wordly-24: поправил ui для чипсов в тёмной теме

--- component/ui/src/main/java/com/nmichail/wordly/android/component/ui/components/ConstructorWordChip.kt
+++ component/ui/src/main/java/com/nmichail/wordly/android/component/ui/components/ConstructorWordChip.kt
@@ -3,6 +3,7 @@ package com.nmichail.wordly.android.component.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +14,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.nmichail.wordly.android.component.ui.theme.WordlyColors
+
 enum class ConstructorWordChipStyle {
 	Bank,
 	Answer,
@@ -26,19 +29,8 @@ fun ConstructorWordChip(
 	modifier: Modifier = Modifier,
 ) {
 	val shape = RoundedCornerShape(12.dp)
-	val background = when (style) {
-		ConstructorWordChipStyle.Bank -> MaterialTheme.colorScheme.surface
-		ConstructorWordChipStyle.Answer -> MaterialTheme.colorScheme.onSurface
-	}
-	val contentColor = when (style) {
-		ConstructorWordChipStyle.Bank -> MaterialTheme.colorScheme.onSurface
-		ConstructorWordChipStyle.Answer -> MaterialTheme.colorScheme.surface
-	}
-	val borderColor = when (style) {
-		ConstructorWordChipStyle.Bank -> MaterialTheme.colorScheme.outline
-		ConstructorWordChipStyle.Answer -> MaterialTheme.colorScheme.onSurface
-	}
-
+	val colorScheme = MaterialTheme.colorScheme
+	val dark = isSystemInDarkTheme()
 	val clickableModifier = if (onClick != null) {
 		Modifier.clickable(onClick = onClick)
 	} else {
@@ -52,11 +44,27 @@ fun ConstructorWordChip(
 			fontSize = 15.sp,
 			lineHeight = 18.sp,
 		),
-		color = contentColor,
+		color = when (style) {
+			ConstructorWordChipStyle.Bank -> colorScheme.onSurface
+			ConstructorWordChipStyle.Answer -> WordlyColors.LightSurface
+		},
 		modifier = modifier
 			.clip(shape)
-			.background(background)
-			.border(1.dp, borderColor, shape)
+			.background(
+				when (style) {
+					ConstructorWordChipStyle.Bank ->
+						if (dark) colorScheme.surfaceVariant else colorScheme.surface
+					ConstructorWordChipStyle.Answer -> WordlyColors.LightOnSurface
+				},
+			)
+			.border(
+				width = 1.dp,
+				color = when (style) {
+					ConstructorWordChipStyle.Bank -> colorScheme.outline
+					ConstructorWordChipStyle.Answer -> WordlyColors.LightOnSurface
+				},
+				shape = shape,
+			)
 			.then(clickableModifier)
 			.padding(horizontal = 14.dp, vertical = 10.dp),
 	)
